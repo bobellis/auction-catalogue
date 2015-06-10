57f09d791c8b42c7b105a18548c1edaa273a1ca2
Ian MacDonald & Bob Ellis <student@epicodus.com>
2015-06-10 12:23:47 -0700

    Add partial functionality for adding a lot.

---
+++ controllers/new-lot.js
@@ -0,0 +1,17 @@
+Auction.NewLotController = Ember.Controller.extend({
+  needs: ['auction'],
+  actions: {
+    save: function () {
+      var newLot = this.store.createRecord({
+        description: this.get('description'),
+        startingBid: this.get('starting-bid')
+      });
+      newLot.save();
+
+      var auction = this.get('controllers.auction.model');
+      auction.get('lots').pushObject(newLot);
+      auction.save();
+      this.transitionToRoute('auction', auction.id);
+    }
+  }
+});
--- models/lot.js
+++ models/lot.js
@@ -1,4 +1,6 @@
 Auction.Lot = DS.Model.extend({
+  description: DS.attr(),
+  startingBid: DS.attr(),
   auction: DS.belongsTo('auction', {async: true}),
   items: DS.hasMany('item', {async: true})
 });
